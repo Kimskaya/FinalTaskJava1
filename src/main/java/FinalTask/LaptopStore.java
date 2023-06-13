3121f1e820c907475858aa8a4657da10411ec5fa
AnnaKim <annakim@inbox.ru>
2023-06-13 19:19:15 +0300

    var1

---
+++ src/main/java/FinalTask/LaptopStore.java
@@ -0,0 +1,44 @@
+package FinalTask;
+import java.util.*;
+public class LaptopStore {
+        private final Set<Laptop> laptops;
+
+        public LaptopStore() {
+            laptops = new HashSet<>();
+        }
+
+        public void addLaptop(Laptop notebook) {
+            laptops.add(notebook);
+        }
+
+        public Set<Laptop> getNotebooks() {
+            return laptops;
+        }
+
+        public Set<Laptop> filterNotebooks(Map<String, String> filters) {
+            Set<Laptop> filteredNotebooks = new HashSet<>(laptops);
+
+            for (Map.Entry<String, String> entry : filters.entrySet()) {
+                String key = entry.getKey();
+                String value = entry.getValue();
+
+                switch (key) {
+                    case "brand":
+                        filteredNotebooks.removeIf(notebook -> !notebook.getBrand().equals(value));
+                        break;
+                    case "color":
+                        filteredNotebooks.removeIf(notebook -> !notebook.getColor().equals(value));
+                        break;
+                    case "price":
+                        filteredNotebooks.removeIf(notebook -> notebook.getPrice() > Integer.parseInt(value));
+                        break;
+                    default:
+                        System.out.println("Вы ввели недопустимые данные");
+                        break;
+                }
+            }
+
+            return filteredNotebooks;
+        }
+    }
+
